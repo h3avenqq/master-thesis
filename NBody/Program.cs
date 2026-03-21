5a69ecf3753711ba531c607b9f213b17bc76c32d
heaven <shmarev082@gmail.com>
2026-03-22 02:22:19 +0300

    NBody_benchmark

--- NBody/Program.cs
+++ NBody/Program.cs
@@ -2,152 +2,177 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
+namespace NBody;
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        var summary = BenchmarkRunner.Run<NBodyBenchmark>();
-    }
+    static void Main(string[] args) => BenchmarkRunner.Run<NBodyBenchmark>();
 }
+
 [MemoryDiagnoser]
 public class NBodyBenchmark
 {
-    [Params(1_000, 5_000, 10_000, 100_000, 1_000_000)]
+    [Params(1_000, 10_000, 100_000)] 
     public int N;
 
     private float _dt;
-    
-    private List<Particle> _oopParticles;
+    private Particle[] _oopParticles;
 
+    // Данные для DOP (Structure of Arrays)
     private float[] _posX;
     private float[] _posY;
     private float[] _posZ;
-    
     private float[] _velX;
     private float[] _velY;
     private float[] _velZ;
-    
+
     [GlobalSetup]
     public void Setup()
     {
         var rand = new Random(42);
         _dt = 0.01f;
 
-        _oopParticles = new List<Particle>();
+        _oopParticles = new Particle[N];
         _posX = new float[N];
         _posY = new float[N];
         _posZ = new float[N];
-        
         _velX = new float[N];
         _velY = new float[N];
         _velZ = new float[N];
-        
+
         for (var i = 0; i < N; i++)
         {
             _posX[i] = rand.NextSingle();
             _posY[i] = rand.NextSingle();
             _posZ[i] = rand.NextSingle();
 
-            _velX[i] = 0;
-            _velY[i] = 0;
-            _velZ[i] = 0;
-            
-            _oopParticles.Add(new Particle
+            _oopParticles[i] = new Particle
             {
                 Position = new Vector3(_posX[i], _posY[i], _posZ[i]),
-                Velocity = new Vector3(_velX[i], _velY[i], _velZ[i])
-            });
+                Velocity = Vector3.Zero
+            };
         }
     }
-    
+
     [Benchmark(Baseline = true)]
     public void Scenario_OOP()
     {
-        for (var i = 0; i < N; i++)
+        float dt = _dt;
+        for (var i = 0; i < _oopParticles.Length; i++)
         {
             Vector3 acceleration = Vector3.Zero;
             var p1 = _oopParticles[i];
 
-            // 1. Считаем сумму сил (Read intensive)
-            for (var j = 0; j < N; j++)
+            for (var j = 0; j < _oopParticles.Length; j++)
             {
                 if (i == j) continue;
                 
                 var p2 = _oopParticles[j];
                 float dist = Vector3.Distance(p1.Position, p2.Position);
-                
-                // Простая модель гравитации/силы: F ~ 1/dist (упрощенно)
-                // Vector3 уже оптимизирован аппаратно!
-                acceleration += new Vector3(1f / dist); 
+                acceleration += new Vector3(1f / (dist + 0.000001f)); 
             }
 
-            // 2. Один раз обновляем позицию (Write once)
-            p1.Velocity += acceleration * _dt;
-            p1.Position += p1.Velocity * _dt;
+            p1.Velocity += acceleration * dt;
+            p1.Position += p1.Velocity * dt;
         }
     }
-    
+
     [Benchmark]
     public void Scenario_DOP_Scalar()
     {
-        // Берем спаны один раз перед циклом (Bound check elimination)
-        var posX = _posX.AsSpan();
-        var posY = _posY.AsSpan();
-        var posZ = _posZ.AsSpan();
-        
-        var velX = _velX.AsSpan();
-        var velY = _velY.AsSpan();
-        var velZ = _velZ.AsSpan();
-        
-        for (var i = 0; i < N; i++)
+        float dt = _dt;
+        var pX = _posX; var pY = _posY; var pZ = _posZ;
+        var vX = _velX; var vY = _velY; var vZ = _velZ;
+
+        for (int i = 0; i < N; i++)
         {
             float ax = 0, ay = 0, az = 0;
-            float p1x = posX[i];
-            float p1y = posY[i];
-            float p1z = posZ[i];
+            float iX = pX[i], iY = pY[i], iZ = pZ[i];
 
-            // 1. Внутренний цикл - самая горячая точка
-            for (var j = 0; j < N; j++)
+            for (int j = 0; j < N; j++)
             {
                 if (i == j) continue;
 
-                // Ручная математика (быстрее чем Math.Pow)
-                float dx = posX[j] - p1x;
-                float dy = posY[j] - p1y;
-                float dz = posZ[j] - p1z;
+                float dx = pX[j] - iX;
+                float dy = pY[j] - iY;
+                float dz = pZ[j] - iZ;
                 
-                // Квадрат расстояния
-                float distSq = dx*dx + dy*dy + dz*dz;
-                
-                // Корень - самая дорогая операция
-                // MathF.Sqrt быстрее чем Math.Sqrt (работает с float)
-                float dist = MathF.Sqrt(distSq);
-                
-                // Сила
+                float dist = MathF.Sqrt(dx * dx + dy * dy + dz * dz + 0.000001f);
                 float f = 1f / dist;
 
-                // Накапливаем ускорение (пока скалярно)
-                ax += f;
-                ay += f;
-                az += f;
+                ax += f; ay += f; az += f;
             }
-            
-            // 2. Обновляем состояние (Запись в память 1 раз на частицу)
-            velX[i] += ax * _dt;
-            velY[i] += ay * _dt;
-            velZ[i] += az * _dt;
-            
-            posX[i] += velX[i] * _dt;
-            posY[i] += velY[i] * _dt;
-            posZ[i] += velZ[i] * _dt;
+
+            vX[i] += ax * dt; vY[i] += ay * dt; vZ[i] += az * dt;
+            pX[i] += vX[i] * dt; pY[i] += vY[i] * dt; pZ[i] += vZ[i] * dt;
+        }
+    }
+
+    [Benchmark]
+    public void Scenario_DOP_SIMD()
+    {
+        float dt = _dt;
+        int vectorSize = Vector<float>.Count; // 8 для AVX2, 4 для SSE
+        float[] pX = _posX; float[] pY = _posY; float[] pZ = _posZ;
+        float[] vX = _velX; float[] vY = _velY; float[] vZ = _velZ;
+
+        Vector<float> epsilon = new Vector<float>(0.000001f);
+
+        for (int i = 0; i < N; i++)
+        {
+            Vector<float> accX = Vector<float>.Zero;
+            Vector<float> accY = Vector<float>.Zero;
+            Vector<float> accZ = Vector<float>.Zero;
+
+            Vector<float> iX = new Vector<float>(pX[i]);
+            Vector<float> iY = new Vector<float>(pY[i]);
+            Vector<float> iZ = new Vector<float>(pZ[i]);
+
+            int j = 0;
+            // Внутренний цикл обрабатывает сразу по 8 частиц
+            for (; j <= N - vectorSize; j += vectorSize)
+            {
+                Vector<float> jX = new Vector<float>(pX, j);
+                Vector<float> jY = new Vector<float>(pY, j);
+                Vector<float> jZ = new Vector<float>(pZ, j);
+
+                Vector<float> dx = jX - iX;
+                Vector<float> dy = jY - iY;
+                Vector<float> dz = jZ - iZ;
+
+                Vector<float> distSq = (dx * dx) + (dy * dy) + (dz * dz) + epsilon;
+                Vector<float> dist = Vector.SquareRoot(distSq);
+                
+                // 1.0f / dist
+                Vector<float> f = Vector<float>.One / dist;
+
+                accX += f; accY += f; accZ += f;
+            }
+
+            // Горизонтальное суммирование накопленного ускорения
+            float totalAx = Vector.Sum(accX);
+            float totalAy = Vector.Sum(accY);
+            float totalAz = Vector.Sum(accZ);
+
+            // Обработка остатка, если N не кратно vectorSize
+            for (; j < N; j++)
+            {
+                if (i == j) continue;
+                float dx = pX[j] - pX[i];
+                float dy = pY[j] - pY[i];
+                float dz = pZ[j] - pZ[i];
+                float f = 1f / (MathF.Sqrt(dx * dx + dy * dy + dz * dz) + 0.000001f);
+                totalAx += f; totalAy += f; totalAz += f;
+            }
+
+            vX[i] += totalAx * dt; vY[i] += totalAy * dt; vZ[i] += totalAz * dt;
+            pX[i] += vX[i] * dt; pY[i] += vY[i] * dt; pZ[i] += vZ[i] * dt;
         }
     }
 }
 
 public class Particle
 {
-    public Vector3 Position { get; set; }
-    public Vector3 Velocity { get; set; }
-
-    public float CalculateForce(Particle other) => 1f / Vector3.Distance(Position, other.Position);
+    public Vector3 Position;
+    public Vector3 Velocity;
 }
